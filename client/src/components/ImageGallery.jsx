938b8d4dc1b31014da2f4b42521b0643ccfd907c
j0ec0de <joelanto386@gmail.com>
2025-08-09 21:43:05 +0530

    experimenting

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -14,6 +14,7 @@ const ImageGallery = () => {
         }
         fetchImages();
     }, []);
+    
 
 
   return (
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getImagesAndVideos } from '../../services/api';
+
+// we do the search function here
+
 
 const SearchBar = () => {
   return (
     <div className="flex justify-center w-full"> {/*the max-w-2xl -- makes it responsive*/ }
       <div className="w-full max-w-2xl bg-gray-200 rounded-2xl shadow-lg px-4 py-2 flex items-center before:border hover:border-2 border-blue-400">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full bg-transparent outline-none text-black placeholder-gray-500"
-        />
+      
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full bg-transparent outline-none text-black placeholder-gray-500"
+
+          />
+      hi
+
       </div>
     </div>
   );
